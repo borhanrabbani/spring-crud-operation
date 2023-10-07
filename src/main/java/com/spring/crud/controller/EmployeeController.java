454ab29520702c974f434174499bfed915c67d5d
Borhan Rabbani <borhanuddinse28@gmail.com>
2023-10-07 23:07:28 +0600

    thymeleaf introduced

--- src/main/java/com/spring/crud/controller/EmployeeController.java
+++ src/main/java/com/spring/crud/controller/EmployeeController.java
@@ -3,20 +3,16 @@ package com.spring.crud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.crud.entity.Employee;
 import com.spring.crud.service.EmployeeService;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 	
 	private EmployeeService employeeService;
@@ -26,45 +22,18 @@ public class EmployeeController {
 		employeeService = theEmployeeService;
 	}
 	
-	@GetMapping("/employees")
-	public List<Employee> findAll(){
-		return employeeService.findAll();
+	
+	
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		List<Employee> theEmployees = employeeService.findAll();
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+
+		return "list-employees";
 	}
 	
-	@GetMapping("/employees/{employeeId}")
-	public Employee getEmployee(@PathVariable int employeeId) {
-		Employee theEmployee = employeeService.findById(employeeId);
-		
-		if(theEmployee==null) {
-			throw new RuntimeException("Employee not found - " + employeeId);
-		}
-		
-		return theEmployee;
-	}
 	
-	@PostMapping("/employees")
-	public Employee addEmployee(@RequestBody Employee theEmployee) {
-		theEmployee.setId(0);
-		Employee dbEmployee = employeeService.save(theEmployee);
-		return dbEmployee;
-	}
-	
-	@PutMapping("/employees")
-	public Employee updateEmployee(@RequestBody Employee theEmployee) {
-		Employee dbEmployee = employeeService.save(theEmployee);
-		return dbEmployee;
-	}
-	
-	@DeleteMapping("/employees/{employeeId}")
-	public String deleteEmployee(@PathVariable int employeeId) {
-		Employee theEmployee = employeeService.findById(employeeId);
-		
-		if(theEmployee==null) {
-			throw new RuntimeException("Employee not found - " + employeeId);
-		}
-		
-		employeeService.deleteById(employeeId);
-		
-		return "Deleted employee id -" + employeeId;
-	}
 }
